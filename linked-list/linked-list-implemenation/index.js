7a9b2f9be82bbed1fe6a92f8bb249dd1ea33bef5
think2code <dheerajsaraswat03@gmail.com>
2026-06-22 21:32:19 +0530

    chore:setup initial implementaion of singly linked list

---
+++ linked-list/linked-list-implemenation/index.js
@@ -0,0 +1,25 @@
+function Node(val){
+    this.val = val;
+    this.next = null;
+}
+
+function myLinkedList(){
+    this.head = null;
+    this.size = 0;
+}
+
+myLinkedList.prototype.get(){
+    
+}
+myLinkedList.prototype.addAtHead(){
+
+}
+myLinkedList.prototype.addAtTail(){
+
+}
+myLinkedList.prototype.addAtIndex(){
+
+}
+myLinkedList.prototype.deleteAtIndex(){
+
+}
