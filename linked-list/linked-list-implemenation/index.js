77f0dfe8a2f25bd570b4c9abdb72d76db8f13c82
think2code <dheerajsaraswat03@gmail.com>
2026-06-23 19:11:30 +0530

    (linked-list): imeplement insertion and get method of linked list

--- linked-list/linked-list-implemenation/index.js
+++ linked-list/linked-list-implemenation/index.js
@@ -1,25 +1,49 @@
-function Node(val){
-    this.val = val;
-    this.next = null;
+function Node(val) {
+  this.val = val
+  this.next = null
 }
 
-function myLinkedList(){
-    this.head = null;
-    this.size = 0;
+function myLinkedList() {
+  this.head = null
+  this.size = 0
 }
 
-myLinkedList.prototype.get(){
-    
+myLinkedList.prototype.get = function (index) {
+  if (index < 0 || index >= this.size) return -1
+  let curr = this.head
+  for (let i = 0; i < index; i++) {
+    curr = curr.next
+  }
+  return curr.val
 }
-myLinkedList.prototype.addAtHead(){
+myLinkedList.prototype.addAtHead = function (val) {
+  let newNode = new Node(val)
+  newNode.next = this.head
+  this.head = newNode
+  this.size++
+}
+myLinkedList.prototype.addAtTail = function (val) {}
+myLinkedList.prototype.addAtIndex = function (index, val) {
+  let newNode = new Node(val)
+  if (index < 0 || index > this.size) return
+  if (index === 0) {
+    this.addAtHead(val)
+    return
+  }
+  if (index === this.size) {
+    this.addAtTail(val)
+    return
+  }
+  let curr = this.head
+  for (let i = 0; i < index - 1; i++) {
+    curr = curr.next
+  }
+  newNode.next = curr.next
+  curr.next = newNode
+  this.size++
+}
+myLinkedList.prototype.deleteAtIndex = function () {}
 
-}
-myLinkedList.prototype.addAtTail(){
+const list = new myLinkedList()
 
-}
-myLinkedList.prototype.addAtIndex(){
-
-}
-myLinkedList.prototype.deleteAtIndex(){
-
-}
+list.get(2)
